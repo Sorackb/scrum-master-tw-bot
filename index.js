a8cf39e1fe5602fca2b668a8f9e85b6509641a32
Sorackb <sorackb@gmail.com>
2021-08-17 11:25:27 -0300

    feat: add lint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const Twit = require('twit');
 
 const scheduled = require('./scheduled');
--- scheduled.js
+++ scheduled.js
@@ -1,27 +1,31 @@
+/* eslint-disable no-console */
 const cron = require('node-cron');
 
 const { identify } = require('./languages');
 const list = require('./cron.json');
 
-const resolve = (err) => {
+const resolve = (err, status) => {
   if (err) {
     console.log(`FAILED: ${err}`);
     return false;
   }
 
   console.error(`POSTED: ${status}`);
-}
+  return true;
+};
 
 const post = (bot, status) => {
   bot.post(
     'statuses/update',
     { status },
-    resolve,
- );
+    (err) => resolve(err, status),
+  );
 };
 
 const start = (bot) => {
-  list.forEach(({ expression, key }) => cron.schedule(expression, () => post(bot, identify(key, process.env.LANGUAGE))));
+  list.forEach(({ expression, key }) => (
+    cron.schedule(expression, () => post(bot, identify(key, process.env.LANGUAGE)))
+  ));
 };
 
 module.exports = {
